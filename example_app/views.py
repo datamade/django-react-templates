6fb3cf59fd3765d3a302b8b1c222d18ae96bf3d5
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-05-11 15:02:25 -0500

    Initialize repo with example project

---
+++ example_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+    title = 'Home'
+    template_name = 'example_pp/index.html'
+    component = 'js/pages/index.js'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['props'] = {'user': 'jean'}  # Set React props here
+        return context
+
+
+def page_not_found(request, exception, template_name='example_app/404.html'):
+    return render(request, template_name, status=404)
+
+
+def server_error(request, template_name='example_app/500.html'):
+    return render(request, template_name, status=500)
