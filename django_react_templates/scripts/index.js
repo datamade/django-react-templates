92e0911c85f722a1fd205bf9534ec49d7cfb9c7e
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-05-14 21:01:05 -0500

    Use browserify to create bundle for client-side hydration

--- django_react_templates/scripts/index.js
+++ django_react_templates/scripts/index.js
@@ -1,16 +1,14 @@
+const templatePath = process.argv[2]
+
+if (!templatePath) {
+  throw new Error('Missing required argument: Path to template')
+}
+
 require('@babel/register')({
   presets: ['@babel/preset-env', '@babel/preset-react']
 })
 
-const templatePath = process.argv[2]
-
-if (!templatePath) {
-  throw new Error('Missing required argument: path to template')
-}
-
+// Load render function from an external module so that we can use babel-register
+// to transpile React components
 const render = require('./render').default
-const html = render(templatePath)
-
-// TODO: Figure out hydration
-
-process.stdout.write(html)
+render(templatePath)
--- django_react_templates/scripts/render.js
+++ django_react_templates/scripts/render.js
@@ -1,10 +1,47 @@
+import stream from 'stream'
+import path from 'path'
+
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import browserify from 'browserify'
+import regeneratorRuntime from "regenerator-runtime";
 
 const render = templatePath => {
   const App = require(templatePath).default
   // TODO: Pass in context here
-  return ReactDOMServer.renderToString(<App />)
+  const appHtml = ReactDOMServer.renderToString(<App />)
+  // TODO: Integrate Helmet for <head> management
+
+  const bundleString = `
+    import React from 'react'
+    import ReactDOM from 'react-dom'
+    import App from '${templatePath}'
+
+    ReactDOM.hydrate(<App />, document.getElementById("root"))
+  `
+
+  const strm = new stream.Transform()
+  strm.push(bundleString)
+  strm.push(null)
+  browserify(strm)
+    .transform('babelify', {presets: ['@babel/preset-env', '@babel/preset-react']})
+    .bundle((err, buf) => {
+      if (err) {
+        // TODO: Better error handling
+        console.log(err)
+        process.exit(1)
+      } else {
+        process.stdout.write(`
+          <!DOCTYPE html>
+          <html>
+            <body>
+              <div id="root">${appHtml}</div>
+            </body>
+            <script type="text/javascript">${buf}</script>
+          </html>`
+        )
+      }
+    })
 }
 
 export default render
